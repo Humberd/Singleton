943cfee97bc0162bcbee2b127e037fd77b5cf864
Humberd <sawik11@wp.pl>
2015-10-02 23:56:43 +0200

    szosty

--- Singleton/src/mojMain.java
+++ Singleton/src/mojMain.java
@@ -14,7 +14,7 @@ public class mojMain {
 		System.out.println(d.get(0));
 		
 		b.set(0, 'f');
-		System.out.println("huwhuhwuhwuhw");
+		System.out.println("zab zupa zebowa dab dupa debowa");
 		System.out.println(c.get(0));
 		System.out.println(d.get(0));
 		
