a287d9b0ec5a1fa5611aa9e393e8a31e54e57dbd
Humberd <sawik11@wp.pl>
2015-10-02 23:48:40 +0200

    trzeci commit

--- Singleton/src/mojMain.java
+++ Singleton/src/mojMain.java
@@ -14,7 +14,7 @@ public class mojMain {
 		System.out.println(d.get(0));
 		
 		b.set(0, 'f');
-		System.out.println("zaladzii");
+		System.out.println("kij ci w oko");
 		System.out.println(c.get(0));
 		System.out.println(d.get(0));
 		
